9a8a8c36a526ea34ba27208e82b11e0605386993
Cameron Labelle <labelle.cam3@gmail.com>
2025-05-27 12:33:08 -0400

    update

--- src/content.ts
+++ src/content.ts
@@ -8,25 +8,28 @@ function waitForElm(selector: string) {
 
         const observer = new MutationObserver(mutations => {
             if (document.querySelector(selector)) {
+                console.log('Found');
                 observer.disconnect();
                 resolve(document.querySelector(selector));
             }
         });
 
         // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+        // this can probably be improved by observing more specific
         observer.observe(document.body, {
             childList: true,
-            subtree: true
+            subtree: true,
         });
     });
 }
-
+console.log('Content script loaded: ' + document.querySelector('#win0divDERIVED_CLSRCH_GROUP6'));
 waitForElm('#win0divDERIVED_CLSRCH_GROUP6').then(() => {
-    console.log('Content script loaded');
+    console.log('Element found');
     const ids = document.querySelectorAll('[id]'); 
     console.log(ids);
     const filtered = Array.from(ids).filter(el => /INSTR\$[0-9]+/.test(el.id))
     console.log(filtered);
-    const names = Array.from(filtered).map(el => (el.textContent ?? '').trim().split('\n')[0]);
+    const names = new Set<string>(filtered.map(el => (el.textContent ?? '').trim().split('\n')[0]));
+    names.delete('');
     console.log(names);
 });
