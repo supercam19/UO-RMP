e35783003f4f3411ea85af25d68cd17cba1c5783
Cameron Labelle <labelle.cam3@gmail.com>
2025-05-27 15:26:01 -0400

    Update

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,23 @@
-function handleMessage(request: object, sender: chrome.runtime.MessageSender, sendResponse: Function) {
-    console.log('Background script received message:', request);
-    sendResponse({ status: 'received' });
-    return true;
+type Packet = {
+    type: string;
+    payload: any;
+}
+
+async function handleMessage(request: Packet, sender: chrome.runtime.MessageSender, sendResponse: Function) {
+    if (request && request.type === 'profNames') {
+        console.log('Background script received message:', request.payload);
+        let ratings = new Map<string, string>();
+        for (const name of request.payload) {
+            const url = "https://www.ratemyprofessors.com/search/professors/1452?q=" + name.replace(" ", "+");
+            try {
+                const res = await fetch(url);
+            } catch (error) {
+                ratings.set(name, 'NA');
+            }
+        }
+        sendResponse({ status: 'received' });
+        return true;
+    }
 }
 
 
