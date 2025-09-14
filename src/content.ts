d6ddc5111d42d453526f9f945acd2cd460e65b89
TheGamerGoat <masterygamer2525@gmail.com>
2025-09-14 01:25:01 -0400

    Added uo grades to the pop up panel

--- src/content.ts
+++ src/content.ts
@@ -23,31 +23,73 @@ function waitForElm(selector: string, waitForExist: boolean = true) {
 }
 
 function onEnrolPage() {
-    const ids = document.querySelectorAll('[id]'); 
-    const filtered = Array.from(ids).filter(el => /^MTG_INSTR\$[0-9]+$/.test(el.id))
-    const names = new Set<string>(filtered.map(el => (parseName(el.textContent) ?? '')));
+    const ids = document.querySelectorAll<HTMLElement>('[id]'); 
+
+    const filteredCourses = Array.from<HTMLElement>(ids).filter(el => /^win0divSSR_CLSRSLT_WRK_GROUPBOX2GP\$[0-9]+/.test(el.id));
+    const coursesMap = new Map<HTMLElement, HTMLElement>(filteredCourses.map(el => {
+        for (let i = 0; i < filteredCourses.length; i++) {
+            const div = el.closest<HTMLElement>(`#win0divSSR_CLSRSLT_WRK_GROUPBOX2\\\$${i}`);
+            if (div != null) return [div,el];
+        }
+        return [el, el];
+    }));
+    
+    const filteredNames = Array.from<HTMLElement>(ids).filter(el => /^MTG_INSTR\$[0-9]+$/.test(el.id));
+    const namesMap = new Map<HTMLElement, HTMLElement>(filteredNames.map(el => {
+        for (let i = 0; i < filteredCourses.length; i++) {
+            const div = el.closest<HTMLElement>(`#win0divSSR_CLSRSLT_WRK_GROUPBOX2\\\$${i}`);
+            if (div != null) return [el,div];
+        }
+        return [el, el];
+    }));
+
+    const encyclopedia = new Map<HTMLElement, Array<string>>(filteredNames.map(el => {
+        const div = namesMap.get(el);
+        if (div == undefined) return [el, []];
+        return [el, [parseName(el.textContent), parseCourse(coursesMap.get(div)!.textContent)]];
+    }));
+
+    const names = new Set<string>(filteredNames.map(el => (parseName(el.textContent) ?? '')));
     names.delete('');
     names.delete('To be Announced');
+
+    const courses = new Set<string>(filteredCourses.map(el => (parseCourse(el.textContent) ?? '')));
+    courses.delete('');
+    console.log(courses);
+
     chrome.runtime.sendMessage(
         {
             type: 'profNames',
-            payload: Array.from(names)
+            payload: [Array.from(names), Array.from(courses)]
         },
         (response) => {
-            for (const el of filtered) {
-                const profName = parseName(el.textContent);
+            console.log(response);
+            for (const el of filteredNames) {
+                const profCoursePair = encyclopedia.get(el);
+                if (profCoursePair == undefined) continue;
+                const profName = profCoursePair[0];
+                const course = profCoursePair[1];
+
                 if (profName === '') continue;
+                if (course === '') continue;
+
+                let courseData = response.payload.courses[course][profName];
+                if (!courseData) courseData = response.payload.courses[course]['None'];
+                const ratings = response.payload.ratings[profName];
+
+                const stats = [GPAToGrade(courseData["mean"]), courseData["median"], courseData["mode"]];
                 el.parentElement?.querySelector('.uormp-rating')?.remove();
                 const div = document.createElement('div');
-                div.textContent = response.payload[profName]['avgRating'] ?? 'NA';
+                div.textContent = ratings['avgRating'] ?? 'NA';
                 div.className = 'uormp-rating';
                 div.style.background = ratingToColour(div.textContent ?? 'NA');
                 div.addEventListener("mouseover", (event) => {
-                    createCard(div, response.payload[profName]['avgRating'], profName,
-                        response.payload[profName]['numRatings'],
-                        response.payload[profName]['wouldTakeAgainPercent'],
-                        response.payload[profName]['avgDifficulty'],
-                        response.payload[profName]['department']
+                    createCard(div, ratings['avgRating'], profName,
+                        ratings['numRatings'],
+                        ratings['wouldTakeAgainPercent'],
+                        ratings['avgDifficulty'],
+                        ratings['department'],
+                        stats
                     )
                 });
                 div.addEventListener("mouseout", (event) => {
@@ -65,7 +107,7 @@ function onEnrolPage() {
     );
 }
 
-function createCard(parentElm: Element, avgRating: string, profName: string, numRatings: string, wouldTakeAgainPercent: string, avgDifficulty: string, department: string) {
+function createCard(parentElm: Element, avgRating: string, profName: string, numRatings: string, wouldTakeAgainPercent: string, avgDifficulty: string, department: string, stats: Array<string>) {
     const card = document.createElement('div');
     card.className = 'uormp-card';
     const parentRect = parentElm.getBoundingClientRect();
@@ -84,6 +126,15 @@ function createCard(parentElm: Element, avgRating: string, profName: string, num
             <div class="uormp-light">${avgDifficulty} average difficulty</div>
         </div>
     `
+    if (stats) {
+        card.innerHTML += `
+            <div class="uogrades-card">
+                <div class="uormp-light">Mean: ${stats[0]}</div>
+                <div class="uormp-light">Median: ${stats[1]}</div>
+                <div class="uormp-light">Mode: ${stats[2]}</div>
+            </div>
+        `
+    }
     parentElm.appendChild(card);
 }
 
@@ -94,6 +145,12 @@ function parseName(str: string | null): string {
     return names[0];
 }
 
+function parseCourse(str: string | null): string {
+    if (!str) return '';
+    str = str.replaceAll(' ', '').toLowerCase().substring(1, 8);
+    return str;
+}
+
 function ratingToColour(rating: string): string {
     if (rating === 'NA') return 'lightgray';
     const num = parseFloat(rating);
@@ -102,6 +159,23 @@ function ratingToColour(rating: string): string {
     if (num >= 3.5) return 'yellow';
     if (num >= 2.5) return 'orange';
     return 'rgb(255, 156, 156)';
+}
+
+function GPAToGrade(GPA: number): string {
+    if (!GPA) return '';
+    const rounded = Math.round(GPA);
+    if (rounded == 10) return 'A+';
+    if (rounded == 9) return 'A';
+    if (rounded == 8) return 'A-';
+    if (rounded == 7) return 'B+';
+    if (rounded == 6) return 'B';
+    if (rounded == 5) return 'C+';
+    if (rounded == 4) return 'C';
+    if (rounded == 3) return 'D+';
+    if (rounded == 2) return 'D';
+    if (rounded == 1) return 'E';
+    return 'F';
+    
 }
 
 async function main() {
